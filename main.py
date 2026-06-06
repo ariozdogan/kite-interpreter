986f3760242c996268d0a2e1aef21c5f9ca85a9a
Ari Ozdogan <ari.ozdogan@gmail.com>
2026-06-06 00:47:33 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+source = None
+path = None
+
+def run(source):
+    return
+
+def run_file(path):
+    file_object = open(path, mode='r', encoding=None)
+    return file_object
+
+def run_prompt():
+    while(True):
+        try:
+            user_arg = input("kite <<<")
+            return user_arg
+        except EOFError:
+            break
+
+def main():
+    
+    return
