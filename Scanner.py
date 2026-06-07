1affa679d8a0893604e305cc58f80ec4ca94b81c
Ari Ozdogan <ari.ozdogan@gmail.com>
2026-06-07 16:53:53 -0700

    organized files

---
+++ Scanner.py
@@ -0,0 +1,18 @@
+
+
+class Scanner:
+  def __init__(self, source):
+    tokens = []
+    self.source = source
+
+
+def isAtEnd():
+  return current >= len(source)
+
+def ScanTokens():
+  while (!isAtEnd()):
+    start = current
+    scanToken()
+
+  
+
--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from enum import Enum, auto
 source = None
 path = None
 
+
 def run(source):
     pass
+
 
 def run_file(path):
     file_object = open(path, mode='r', encoding=None)
     return file_object
+
 
 def run_prompt():
     while(True):
@@ -19,8 +22,10 @@ def run_prompt():
         except EOFError:
             break
 
+
 def error(line, message):
     report(line, "", message)
+
 
 def report(line, where, message):
     print(where + ", line: " + line)
@@ -28,57 +33,7 @@ def report(line, where, message):
 
     hadError = True
 
-class TokenType(Enum):
-    # single-character tokens
-    LEFT_PAREN  = auto()
-    RIGHT_PAREN = auto()
-    LEFT_BRACE  = auto()
-    RIGHT_BRACE = auto()
-    COMMA       = auto()
-    DOT         = auto()
-    MINUS       = auto()
-    PLUS        = auto()
-    STAR        = auto()
-    SLASH       = auto()
-    SEMICOLON   = auto()
-
-    # one/two character tokens
-    BANG          = auto()
-    BANG_EQUAL    = auto()
-    EQUAL         = auto()
-    EQUAL_EQUAL   = auto()
-    GREATER       = auto()
-    GREATER_EQUAL = auto()
-    LESS          = auto()
-    LESS_EQUAL    = auto()
-
-    # literals
-    IDENTIFIER = auto()
-    STRING     = auto()
-    NUMBER     = auto()
-
-    # keywords
-    AND    = auto()
-    CLASS  = auto()
-    ELSE   = auto()
-    FALSE  = auto()
-    FUN    = auto()
-    FOR    = auto()
-    IF     = auto()
-    NULL   = auto()
-    OR     = auto()
-    PRINT  = auto()
-    RETURN = auto()
-    SUPER  = auto()
-    THIS   = auto()
-    TRUE   = auto()
-    STR    = auto()
-    INT    = auto()
-    FLOAT  = auto()
-    BOOL   = auto()
-    WHILE  = auto()
-
-    EOF = auto()
+    
 
 def main():
     if sys.argv > 2:
